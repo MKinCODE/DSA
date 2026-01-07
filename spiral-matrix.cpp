436ec62adc5598357018886befbb5ed44504dfff
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-01-07 16:30:33 +0000

    change in file

--- spiral-matrix.cpp
+++ spiral-matrix.cpp
@@ -11,24 +11,24 @@ public:
         int bottom=matrix.size()-1;
         vector<int> ans;
         while(left<=right && top<=bottom){
-
+            // traverse from left to right and increase top by one
             for(int i=left; i<=right;i++){
                 ans.push_back(matrix[top][i]);
             }
             top++;
-            
+            //traverse from top to bottom and decrease right
             for(int i=top; i<=bottom;i++){
                 ans.push_back(matrix[i][right]);
             }
             right--;
-
+            //if condition satisfies traverse from right to left and decrease bottom
             if(top<=bottom){
                 for(int i=right; i>=left ;i--){
                 ans.push_back(matrix[bottom][i]);
             }
             bottom--;
             }
-
+            //if condition satisfies traverse from bottom to top and increase left
             if(left<=right){
                 for(int i=bottom; i>=top ;i--){
                 ans.push_back(matrix[i][left]);
