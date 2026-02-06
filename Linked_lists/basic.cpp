386a00ba7b327cabf06f52b564025476f90f54e6
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-02-06 16:57:19 +0000

    basic LL

--- Linked_lists/basic.cpp
+++ Linked_lists/basic.cpp
@@ -6,8 +6,57 @@ class Node{
     Node* next;
     Node(int x){value=x;next=NULL;}
 };
+void traverse(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->value<<" ";
+        temp=temp->next;
+    }
+}
+int count(Node* head){
+    Node* temp=head;
+    int counter=0;
+    while(temp!=NULL){
+        temp=temp->next;
+        counter++;
+    }
+    return counter;
+}
+void insert_node(Node* &head){
+    int n=count(head);
+    int data,pos;
+    cout<<"enter value to insert:";
+    cin>>data;
+    Node* newNode= new Node(data);
+    cout<<"enter position to insert: ";
+    cin>>pos;
+    if(pos<1 || pos>n+1){cout<<"Invalid position";return;}
+    if(pos==1){
+        newNode->next=head;
+        head=newNode;
+        return;
+    }
+    if(pos==n+1){
+        Node*temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        return;
+    }
+    Node* temp=head;
+    for(int i=1;i<pos-1;i++){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    
+}
 int main(){
-    Node* head = new Node(5);
-    cout<<endl<<head->value<<endl;
+    Node* head=NULL;
+
+    insert_node(head);
+    traverse(head);
+    cout<<endl<<"Total nodes in LL: "<<count(head)<<endl;
     return 0;
 }
