aefcc3048891b5c6c716d068ddb27b83834d920e
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-02-08 14:36:24 +0000

    leetcode version

--- Linked_lists/basic.cpp
+++ Linked_lists/basic.cpp
@@ -85,3 +85,156 @@ int main(){
     cout<<"Node deleted at the position with value: "<<delval<<endl;
     return 0;
 }
+
+//leetcode
+// class Node{
+//     public:
+//     int val;
+//     Node* next;
+//     Node(int x){val=x;next=NULL;}
+// };
+// class MyLinkedList {
+// private:
+//     Node* head;
+// public:
+//     MyLinkedList() {
+//         head=NULL;
+//     }
+    
+//     int get(int index) {
+//         Node* temp= head;
+//         for(int i=0;i<index && temp!=NULL;i++){
+//             temp=temp->next;
+//         }
+//         if(temp==NULL) return -1;
+//         return temp->val;
+//     }
+    
+//     void addAtHead(int val) {
+//         Node* newNode = new Node(val);
+//         newNode->next=head;
+//         head=newNode;
+//     }
+    
+//     void addAtTail(int val) {
+//         Node* newNode = new Node(val);
+//         if(head==NULL){head=newNode;
+//         return;}
+//         Node*temp=head;
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//         }
+//         temp->next=newNode;
+//     }
+    
+//     void addAtIndex(int index, int val) {
+//         if(index<0) return;
+//         if(index==0){addAtHead(val);return;}
+//         Node* newNode = new Node(val);
+//         Node*temp=head;
+//         for(int i=0;i<index-1 && temp!=NULL;i++){
+//             temp=temp->next;
+//         }
+//         if(temp==NULL) return;
+//         newNode->next=temp->next;
+//         temp->next=newNode;
+//     }
+    
+//     void deleteAtIndex(int index) {
+//         if(index<0 || head==NULL) return;
+//         Node*temp=head;
+//         if(index==0){Node* ndel=head;
+//         head=head->next;
+//         delete ndel;
+//         return;
+//         }
+//         for(int i=0;i<index-1 && temp->next!=NULL;i++){
+//             temp=temp->next;
+//         }
+//         if(temp==NULL || temp->next==NULL) return;
+//         Node* del=temp->next;
+//         temp->next=temp->next->next;
+//         delete del;
+//     }
+// };
+
+
+
+//using size
+// class Node{
+//     public:
+//     int val;
+//     Node* next;
+//     Node(int x){val=x;next=NULL;}
+// };
+// class MyLinkedList {
+// private:
+//     Node* head;
+//     int size;
+// public:
+//     MyLinkedList() {
+//         head=NULL;
+//         size=0;
+//     }
+    
+//     int get(int index) {
+//         if(index<0 || index>=size) return -1;
+//         Node* temp= head;
+//         for(int i=0;i<index;i++){
+//             temp=temp->next;
+//         }
+//         return temp->val;
+//     }
+    
+//     void addAtHead(int val) {
+//         Node* newNode = new Node(val);
+//         newNode->next=head;
+//         head=newNode;
+//         size++;
+//     }
+    
+//     void addAtTail(int val) {
+//         Node* newNode = new Node(val);
+//         if(head==NULL){head=newNode;size++;
+//         return;}
+//         Node*temp=head;
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//         }
+//         temp->next=newNode;
+//         size++;
+//     }
+    
+//     void addAtIndex(int index, int val) {
+//         if(index<0 || index>size) return;
+//         if(index==0){addAtHead(val);return;}
+//         Node* newNode = new Node(val);
+//         Node*temp=head;
+//         for(int i=0;i<index-1;i++){
+//             temp=temp->next;
+//         }
+//         newNode->next=temp->next;
+//         temp->next=newNode;
+//         size++;
+//     }
+    
+//     void deleteAtIndex(int index) {
+//         if(index<0 || index>=size) return;
+//         Node*temp=head;
+//         if(index==0){Node* ndel=head;
+//         head=head->next;
+//         delete ndel;
+//         size--;
+//         return;
+//         }
+//         for(int i=0;i<index-1;i++){
+//             temp=temp->next;
+//         }
+//         Node* del=temp->next;
+//         temp->next=temp->next->next;
+//         delete del;
+//         size--;
+//     }
+// };
+
+
