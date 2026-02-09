9b802285d7d0565aa1fb137430a01a211449a5b1
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-02-09 14:19:44 +0000

    problem

---
+++ Linked_lists/delete_node.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp= node->next;
+        node->val=node->next->val;
+        node->next=node->next->next;
+        delete temp;
+    }
+};
