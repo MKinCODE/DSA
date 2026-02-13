1a1625e5621b109c68447fcd6cb2c0147b513ef3
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-02-13 14:40:02 +0000

    optimal solution added

--- Linked_lists/max_pair_sum.cpp
+++ Linked_lists/max_pair_sum.cpp
@@ -31,3 +31,48 @@ public:
         return ans;
     }
 };
+
+// optimistic solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){return 0;}
+        ListNode* slow=head;
+        ListNode* prevslow=nullptr;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            prevslow=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* curr=slow;
+        ListNode* prev=nullptr;
+        ListNode* nextptr=nullptr;
+        // prevslow->next=nullptr;  no need to separate still it works
+        while(curr!=nullptr){
+            nextptr=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextptr;
+        }
+       // prevslow->next=prev; no need to join them again
+        ListNode*temp=head;
+        int ans=0;
+        while(prev!=nullptr){   // we have to use prev cause it is the first node of second half
+            ans=max(ans,((temp->val)+(prev->val)));
+            prev=prev->next;
+            temp=temp->next;
+        }
+        return ans;
+    }
+};
