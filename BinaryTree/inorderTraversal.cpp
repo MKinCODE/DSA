c02097ab2753f174a85e211123edc7b977529261
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-05-18 16:54:39 +0000

    new problems

--- BinaryTree/inorderTraversal.cpp
+++ BinaryTree/inorderTraversal.cpp
@@ -9,36 +9,34 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        if(root==nullptr) return{};
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==nullptr) return {};
         vector<int> ans;
         vector<int> leftp;
         vector<int> rightp;
+        leftp=inorderTraversal(root->left);
         ans.push_back(root->val);
-        leftp=preorderTraversal(root->left);
-        rightp=preorderTraversal(root->right);
-        ans.insert(ans.end(),leftp.begin(),leftp.end());
-        ans.insert(ans.end(),rightp.begin(),rightp.end());
-        return ans;
-
+        rightp=inorderTraversal(root->right);
+        leftp.insert(leftp.end(),ans.begin(),ans.end());
+        leftp.insert(leftp.end(),rightp.begin(),rightp.end());
+        return leftp;
     }
 };
 
 //cleaner version: without using multiple vectors
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        preorder(root,ans);
+        inorder(root,ans);
         return ans;
     }
-    void preorder(TreeNode* root, vector<int>& ans){
+    void inorder(TreeNode* root, vector<int>& ans){
         if(root==nullptr) return;
+        inorder(root->left,ans);
+        inorder(root->right,ans);
         ans.push_back(root->val);
-        preorder(root->left,ans);
-        preorder(root->right,ans);
     }
 };
