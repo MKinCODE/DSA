c854f479331ca8147ea8fb67c1ff911b278c7d6c
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-06-09 08:19:44 +0000

    added cleaner version

--- backtracking/numbers_with_same_consecutive_difference.cpp
+++ backtracking/numbers_with_same_consecutive_difference.cpp
@@ -30,3 +30,31 @@ public:
         return result;
     }
 };
+
+//cleaner:
+class Solution {
+private:
+    vector<int> result;
+public:
+    void helper(string& path, int n, int k){
+        if(path.length()==n){
+            result.push_back(stoi(path));
+            return;
+        }
+        string nums;
+        if(path.length()==0) nums="123456789";
+        else nums="0123456789";
+
+        for(char a:nums){
+            path+=a;
+            if(path.length()==1 || abs(path[path.length()-1]-path[path.length()-2])==k) helper(path,n,k);
+            path.pop_back();
+        }
+
+    }
+    vector<int> numsSameConsecDiff(int n, int k) {
+        string path="";
+        helper(path,n,k);
+        return result;
+    }
+};
