b92ecc465575a6e95287ecd43031c51e2e39d642
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-06-17 16:27:52 +0000

    new problem

---
+++ Greedy Algorithms/max_matching_of_players_with_trainee.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int n=players.size();
+        int m=trainers.size();
+        int i=0,j=0,ans=0;
+        while(i<n && j<m){
+            if(trainers[j]>=players[i]){
+                ans++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return ans;
+    }
+};
