f47da19c6690fbbba6cf40feb6b28e27013531b5
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-07-01 16:42:21 +0000

    new problem

---
+++ Heap/Kth_largest_element_in_array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size()-1;
+        if(k>n+1) return -1;
+        priority_queue<int,vector<int>,greater<int>> minHeap;
+        for(int x:nums){
+            minHeap.push(x);
+
+            if(minHeap.size()>k) 
+                minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
