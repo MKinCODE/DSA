3ce23a876b4da301a18231fb9c0b9bef06fc4f2a
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-08-06 13:16:11 +0000

    another sol

--- BinaryTree/dfs/path_sum_III.cpp
+++ BinaryTree/dfs/path_sum_III.cpp
@@ -1,4 +1,5 @@
 #include<queue>
+#include<unordered_map>
 using namespace std;
 struct TreeNode {
     int val;
@@ -8,6 +9,44 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+//most optimistic approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int count=0;
+    unordered_map<long long ,int> mp;
+public:
+    void dfs(TreeNode* root, int targetSum, long long prefix){
+        if(root==nullptr) return;
+
+        prefix+=root->val;
+
+        count+=mp[prefix-targetSum];  //it works without checking it exists as [] return 0 if key is not found
+
+        mp[prefix]++;
+        dfs(root->left,targetSum,prefix);
+        dfs(root->right,targetSum,prefix);
+        mp[prefix]--;
+        if(mp[prefix]==0) mp.erase(prefix);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root==nullptr) return 0;
+        mp[0]=1;
+        dfs(root,targetSum,0);
+
+        return count;
+    }
+};
+
 //using dfs as outer traversal
 class Solution {
     int count=0;
