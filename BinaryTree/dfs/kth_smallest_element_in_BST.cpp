5e3d00f05bbf193d679173a42ba305967f77a57e
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-08-10 15:50:04 +0000

    new problem

---
+++ BinaryTree/dfs/kth_smallest_element_in_BST.cpp
@@ -0,0 +1,28 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    int count=0;
+    int ans;
+public:
+    void dfs(TreeNode* root, int k){
+        if(root==nullptr) return;
+        dfs(root->left,k);
+        count++;
+        if(count==k){
+            ans=root->val;
+            return;
+        }
+        dfs(root->right,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        dfs(root,k);
+        return ans;
+    }
+};
